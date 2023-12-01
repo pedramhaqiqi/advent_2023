fe69ae94fc55653f5b3d9e1d65c03a5b6d9425cb
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2023-12-01 14:51:27 -0500

    Initial

--- q1/q1.py
+++ q1/q1.py
@@ -3,11 +3,23 @@ def read_file(filename):
         for line in file:
             yield line.strip()
 
+def find_number_in_line(line ,reverse=False):
+    if reverse:
+        for i in range(len(line)-1, -1, -1):
+            if line[i].isdigit():
+                return (i, line[i])
+    else:
+        for i in range(len(line)):
+            if line[i].isdigit():
+                return (i, line[i])
+    
 
 
 def main():
     for line in read_file('q1.txt'):
-        pass
+        result = find_number_in_line(line)
+        print(result)
+        return
 
 if __name__ == "__main__":
     main()
